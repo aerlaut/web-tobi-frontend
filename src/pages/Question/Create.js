ef8648baa6a27fb3fd03d3a23e705b1d5a2f777f
Anugerah Erlaut <aerlaut@live.com>
2020-08-08 15:52:42 +0700

    create meta inputs for question

--- src/pages/Question/Create.js
+++ src/pages/Question/Create.js
@@ -5,7 +5,37 @@ import Error from '../../components/Error'
 
 export default function () {
   const history = useHistory()
-  const [error, setError] = useState()
+  const [error, setError] = useState('')
+  const [author, setAuthor] = useState('')
+  const [tier, setTier] = useState('')
+  const [maxScore, setMaxScore] = useState('')
+  const [official, setOfficial] = useState(true)
+  const [difficulty, setDifficulty] = useState('')
+
+  const tiers = [
+    {
+      value: 'osk',
+      name: 'OSK',
+    },
+    {
+      value: 'osp',
+      name: 'OSP',
+    },
+    {
+      value: 'osn',
+      name: 'OSN',
+    },
+    {
+      value: 'pelatnas',
+      name: 'Pelatnas',
+    },
+    {
+      value: 'ibo',
+      name: 'IBO',
+    },
+  ]
+
+  let question = {}
 
   useEffect(() => {
     // Fetch dashboard data
@@ -23,6 +53,76 @@ export default function () {
       <>
         {error && <Error type={error.type} message={error.message} />}
         <h1 className="text-xl font-bold mb-4">Buat Soal Baru</h1>
+
+        <div className="flex">
+          <div className="w-1/2 flex flex-col">
+            <label className="my-2">
+              <span className="w-2/12 inline-block">Author</span>
+              <input
+                type="text"
+                className="w-2/12 p-1 border border-black shadow-inside rounded"
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+              ></input>
+            </label>
+
+            <label className="my-2">
+              <span className="w-2/12 inline-block">Tier</span>
+              <select
+                onChange={(e) => setTier(e.target.value)}
+                value={tier}
+                className="border rounded p-1 border-black w-2/12"
+              >
+                {tiers.map((t) => (
+                  <option value={t.value}>{t.name}</option>
+                ))}
+              </select>
+            </label>
+
+            <label className="my-2">
+              <span className="w-2/12 inline-block">Official?</span>
+              <select
+                value={official}
+                onChange={(e) => setOfficial(official ? false : true)}
+                className="border rounded p-1 border-black w-2/12"
+              >
+                <option value={true}>Yes</option>
+                <option value={false}>No</option>
+              </select>
+            </label>
+          </div>
+          <div className="w-1/2 flex flex-col">
+            <label className="my-2">
+              <span className="w-2/12 inline-block">Difficulty</span>
+              <input
+                type="range"
+                className="w-2/12 p-1 border border-black shadow-inside rounded"
+                min="1"
+                max="5"
+                value={difficulty}
+                onChange={(e) => setDifficulty(e.target.value)}
+              ></input>
+            </label>
+
+            <label className="my-2">
+              <span className="w-2/12 inline-block">Maximum Score</span>
+              <input
+                type="text"
+                className="w-1/12 p-1 border border-black shadow-inside rounded"
+                value={maxScore}
+                onChange={(e) => setMaxScore(e.target.value)}
+              ></input>
+            </label>
+          </div>
+        </div>
+        <div>
+          <strong>Topics</strong>
+          <div class="rounded border" style={{ minHeight: 2 + 'em' }}></div>
+        </div>
+        <div>
+          <strong>Subtopics</strong>
+          <div class="rounded border" style={{ minHeight: 2 + 'em' }}></div>
+        </div>
       </>
     )
   )
