29159d8adabb36cc6b5b9e9e9efc4c6b9b99acd4
Anugerah Erlaut <aerlaut@live.com>
2020-08-22 18:17:29 +0700

    update Text state

--- src/components/Question/Text.js
+++ src/components/Question/Text.js
@@ -1,18 +1,38 @@
 import React, { useState } from 'react'
-import { EditorState, ContentState } from 'draft-js'
+import { EditorState, ContentState, convertToRaw } from 'draft-js'
 import { Editor } from 'react-draft-wysiwyg'
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+import { useDispatch } from 'react-redux'
 
-export default function (props) {
+export default function ({content, idx}) {
+
+  let type = 'question_text'
+  const dispatch = useDispatch()
+
   const [editorState, setEditorState] = useState(() => {
-    if (props.content !== '') {
+    if (content !== '') {
       return EditorState.createWithContent(
-        ContentState.createFromText(props.content)
+        ContentState.createFromText(content)
       )
     } else {
       return EditorState.createEmpty()
     }
   })
+
+  function handleEditorChange(e) {
+
+    dispatch({
+      type: 'question/updateField',
+      payload: {
+        idx: idx,
+        type: type,
+        content: e.getCurrentContent().getPlainText(),
+        },
+    })
+
+    setEditorState(e)
+
+  }
 
   return (
     <Editor
@@ -20,7 +40,7 @@ export default function (props) {
       wrapperClassName="min-h-1/4 my-2"
       toolbarClassName="border border-gray-800 rounded-t mb-0"
       editorClassName="border border-gray-800 rounded-b px-4"
-      onEditorStateChange={setEditorState}
+      onEditorStateChange={(e) => handleEditorChange(e)}
     />
   )
 }
