8032c5c4e4f0f62a078ac529724b65b62857ff62
Anugerah Erlaut <aerlaut@live.com>
2020-09-20 09:49:31 +0700

    show question set in index

--- src/pages/Question/Index.js
+++ src/pages/Question/Index.js
@@ -5,11 +5,13 @@ import Error from '../../components/Error'
 import NewItem from '../../icons/NewItem'
 import QuestionCard from '../../components/Question/Card'
 import { useSelector } from 'react-redux'
+import { queryByDisplayValue } from '@testing-library/react'
 
 export default function () {
 	const history = useHistory()
 	const [error, setError] = useState()
 	const [questions, setQuestions] = useState([])
+	const [questionSets, setQuestionSets] = useState([])
 	const role = useSelector((state) => state.auth.role)
 
 	useEffect(() => {
@@ -18,8 +20,10 @@ export default function () {
 			if (res.status !== 'ok') {
 				setError({ type: 'error', message: res.message })
 			} else {
+				console.log(res.data)
 				// Setting information
-				setQuestions(res.data)
+				setQuestions(res.data.questions)
+				setQuestionSets(res.data.question_sets)
 			}
 		})
 	}, [])
@@ -51,6 +55,7 @@ export default function () {
 					<div className='mb-4'>Search</div>
 					<div className='flex flex-wrap'>
 						{questions.map((q, idx) => (
+							// <>{q.id}, </>
 							<QuestionCard
 								question={q}
 								key={`q_${q.id}`}
@@ -63,9 +68,7 @@ export default function () {
 				</div>
 
 				<div>
-					<h1 className='text-xl font-bold mb-4 pt-8 pb-4 inline-block'>
-						New Sets
-					</h1>
+					<h1 className='text-xl font-bold mb-4 pt-8 inline-block'>New Sets</h1>
 					{role === 'admin' || role === 'superadmin' ? (
 						<span className='float-right inline-block'>
 							<Link
@@ -83,6 +86,19 @@ export default function () {
 					) : (
 						''
 					)}
+
+					<div className='mb-4'>Search</div>
+					<div className='flex flex-wrap'>
+						{questionSets.map((qs, idx) => (
+							<QuestionCard
+								question={qs}
+								key={`qs_${qs.id}`}
+								onClick={() => {
+									history.push(`/question_set/${qs.id}`)
+								}}
+							/>
+						))}
+					</div>
 
 					{/* Show question set here */}
 				</div>
